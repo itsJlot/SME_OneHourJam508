6f21747ea755d2b820e9d9f135f286a090a5f662
donkeyrat <darklordbananas@gmail.com>
2025-01-18 12:13:31 -0800

    game is now playable

--- Assets/Scripts/Props/NpcBehavior.cs
+++ Assets/Scripts/Props/NpcBehavior.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Props;
 using UnityEngine;
 
 public class NpcBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Random.value > 0.5)
+        {
+            var spawnedHat = Instantiate(hats[Random.Range(0, hats.Length - 1)], hatPos);
+            var prop = spawnedHat.GetComponent<PropBehavior>();
+            prop.rig.isKinematic = true;
+            prop.canBeInteractedWith = false;
+        }
+        if (Random.value > 0.5)
+        {
+            var spawnedGlasses = Instantiate(glasses[Random.Range(0, hats.Length - 1)], glassesPos);
+            var prop = spawnedGlasses.GetComponent<PropBehavior>();
+            prop.rig.isKinematic = true;
+            prop.canBeInteractedWith = false;
+        }
+        if (Random.value > 0.5)
+        {
+            var spawnedCup = Instantiate(cups[Random.Range(0, hats.Length - 1)], cupPos);
+            var prop = spawnedCup.GetComponent<PropBehavior>();
+            prop.rig.isKinematic = true;
+            prop.canBeInteractedWith = false;
+        }
     }
 
     public Transform hatPos;
     public Transform glassesPos;
     public Transform cupPos;
+
+    public GameObject[] hats;
+    public GameObject[] glasses;
+    public GameObject[] cups;
 }
